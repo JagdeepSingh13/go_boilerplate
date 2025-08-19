888345b6f186e203980122aa7b1a89dfa685dd5e
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-08-19 14:38:17 +0530

    main file and running

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -29,7 +29,7 @@ type ServerConfig struct {
 	Port               string   `koanf:"port" validate:"required"`
 	ReadTimeout        int      `koanf:"read_timeout" validate:"required"`
 	WriteTimeout       int      `koanf:"write_timeout" validate:"required"`
-	IdleTimeout        int      `kaonf:"idle_timeout" validate:"required"`
+	IdleTimeout        int      `koanf:"idle_timeout" validate:"required"`
 	CORSAllowedOrigins []string `koanf:"cors_allowed_origins" validate:"required"`
 }
 
